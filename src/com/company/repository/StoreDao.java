cd2c8697f65ed143a71eee38dc41d8026f5ef9ce
Vlad <vladyaskelevich@gmail.com>
2021-08-10 21:33:44 +0300

    Fixed getById

--- src/com/company/repository/StoreDao.java
+++ src/com/company/repository/StoreDao.java
@@ -2,8 +2,11 @@ package com.company.repository;
 
 import com.company.models.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StoreDao extends JpaRepository<Store,Integer> {
+
+    @Override
+    @Query("SELECT s FROM Store s WHERE s.id = ?1")
+    Store getById(Integer integer);
 }
